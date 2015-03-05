ff67f78c83ad4acab264b0b1597072e2c6290095
elizabeth perez gonzalez <elizabeth.perezgonzalez@gmail.com>
2015-03-04 16:18:51 -0800

    Incomplete associations assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ActiveRecord::Base
+  scope :ordered_by_title, -> { order(:title, :asc) }
+
   has_many :comments
   belongs_to :user
 
-  default_scope { order ('created_at DESC') }
 end
