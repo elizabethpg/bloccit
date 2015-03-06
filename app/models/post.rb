992333a02bbff37734681a95b1b92d96bc3feaa4
elizabeth perez gonzalez <elizabeth.perezgonzalez@gmail.com>
2015-03-06 13:26:24 -0800

    Complete Associations assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,10 @@
 class Post < ActiveRecord::Base
-  scope :ordered_by_title, -> { order(:title, :asc) }
+  scope :ordered_by_title, -> { order("title ASC") }
+  scope :ordered_by_reverse_created_at, -> { order(:created_at).reverse_order }
 
   has_many :comments
   belongs_to :user
+
+  default_scope { order('created_at DESC') }
 
 end
