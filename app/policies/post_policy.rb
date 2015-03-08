99f95bbc1ec517171698a98ecc38ebf5e016537a
elizabeth perez gonzalez <elizabeth.perezgonzalez@gmail.com>
2015-03-07 17:31:27 -0800

    Complete Authorization assignment

--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -1,5 +1,20 @@
 class PostPolicy < ApplicationPolicy
 
+  class Scope < Scope
+
+    def resolve
+      if @user
+        if user.admin? || user.moderator?
+          scope.all
+        else
+          scope.where(:user => user)
+        end
+      else
+        scope.none
+      end
+    end
+  end
+
   def index?
     true
   end
