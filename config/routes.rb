57a8aef76e30958e733da9eddeda6965c6600c42
elizabeth perez gonzalez <elizabeth.perezgonzalez@gmail.com>
2015-03-10 07:58:04 -0700

    complete topics and posts assignment

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,11 @@ Rails.application.routes.draw do
   resources  :topics do
     resources :posts, except: [:index]
   end
-  resources :summaries, except: [:index, :new]
 
+  resources :posts do
+    resources :summaries, except: [:index, :new]
+  end
+  
   resources :advertisements
 
   get 'about' => 'welcome#about'
