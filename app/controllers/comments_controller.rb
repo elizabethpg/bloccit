492945bbf9f5e263cc289f16ae9155b07190ab0b
elizabeth perez gonzalez <elizabeth.perezgonzalez@gmail.com>
2015-03-14 16:52:33 -0700

    Completed destroy assignment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,22 +1,22 @@
 class CommentsController < ApplicationController
 
   def destroy
-    @topic = Topic.find(params[:topic_id])
-    @post = @topic.posts.find(params[:post_id])
+    # @topic = Topic.find(params[:topic_id])
+    @post = Post.find(params[:post_id])
     @comment = @post.comments.find(params[:id])
 
     authorize @comment
     if @comment.destroy
       flash[:notice] = "Comment was removed."
-      redirect_to [@topic, @post]
+      redirect_to [@post.topic, @post]
      else
       flash[:error] = "Comment couldn't be deleted. Try again."
-      redirect_to [@topic, @post]
+      redirect_to [@post.topic, @post]
     end
   end
 
   def create
-    @topic = Topic.find(params[:topic_id])
+    # @topic = Topic.find(params[:topic_id])
     @post = Post.find(params[:post_id])
     # @comment = @post.comments.create(comment_params)
     @comments = @post.comments
@@ -26,7 +26,7 @@ class CommentsController < ApplicationController
     authorize @post
       if @comment.save
         flash[:notice] = "Comment was saved."
-        redirect_to [@topic, @post]
+        redirect_to [@post.topic, @post]
       else
         flash[:error] = "There was an error saving the comment. Please try again."
         render :new
